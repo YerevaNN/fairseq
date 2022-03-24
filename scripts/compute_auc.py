30e5a514feff9bb01762675c07672e48a419fc84
gayanechilingar <gayane.chilingar@gmail.com>
2022-03-24 13:53:48 +0400

    * Compute AUC for regression * Add datasets.json for hyperparams * Fix the preprocessing

--- scripts/compute_auc.py
+++ scripts/compute_auc.py
@@ -4,62 +4,71 @@ from fairseq.data import Dictionary
 from sklearn.metrics import roc_auc_score, classification_report, mean_squared_error
 from tqdm import tqdm
 import os
+import json
 import torch
 
 # bbbp
-model = "/home/gayane/BartLM/Bart/chemical/checkpoints/chemical/checkpoints/ft.bart_large.sentpred.ms16.uf1.mu2296.dr0.1.atdr0.1.actdr0.0.wd0.01.adam.beta9999.eps1e-08.clip0.1.lr3e-05.warm367.fp16.ngpu8/"
-dataset = "bbbp"
 
-# model = "/home/gayane/BartLM/Bart/chemical/checkpoints/chemical/checkpoints/ft_clintox.bart_large.sentpred.ms16.uf1.mu2296.dr0.1.atdr0.1.actdr0.0.wd0.01.adam.beta9999.eps1e-08.clip0.1.lr3e-05.warm367.fp16.ngpu8"
-# dataset = "clintox"
+dataset = "freesolv"
+model = f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}/processed"
 
-# model = "/home/gayane/BartLM/Bart/chemical/checkpoints/chemical/checkpoints/ft_tox21.bart_large.sentpred.ms16.uf1.mu2296.dr0.1.atdr0.1.actdr0.0.wd0.01.adam.beta9999.eps1e-08.clip0.1.lr3e-05.warm367.fp16.ngpu8/"
-# dataset = "tox21"
+with open('/home/gayane/BartLM/fairseq/scripts/datasets.json') as f:
+    datasets_json = json.load(f)
 
-os.system(f"mkdir {model}/input0")
-os.system(f"mkdir {model}/label")
+dataset_js = datasets_json[dataset]
+task_type = dataset_js['type']
+mi = dataset_js['minimum']
+ma = dataset_js['maximum']
 
-# os.system(f"cp /home/gayane/BartLM/Bart/chemical/checkpoints/chemical/evaluation_data/{dataset}/processed/input0/dict.txt {model}/input0/")
-# os.system(f"cp /home/gayane/BartLM/Bart/chemical/checkpoints/chemical/evaluation_data/{dataset}/processed/label/dict.txt {model}/label/")
+os.system(f"mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}/")
+os.system(f"mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}/processed/")
+os.system(f"mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}/processed/input0/")
+os.system(f"mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}/processed/label/")
 
-os.system(f"cp /home/gayane/BartLM/fairseq/processed/input0/dict.txt {model}/input0/")
-os.system(f"cp /home/gayane/BartLM/fairseq/processed/label/dict.txt {model}/label/")
+# os.system(f"cp /home/gayane/BartLM/{dataset}/processed/input0/dict.txt /home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}/processed/input0/")
+# os.system(f"cp /home/gayane/BartLM/{dataset}/processed/label/dict.txt /home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}processed/label/")
 
-bart = BARTModel.from_pretrained(model, checkpoint_file='/home/gayane/BartLM/checkpoints/checkpoint_best.pt', 
+bart = BARTModel.from_pretrained(model, checkpoint_file =f'/home/gayane/BartLM/checkpoints/{dataset}/checkpoint.best_loss_0.2400.pt', 
                                  bpe="sentencepiece",
-                                 sentencepiece_model="/home/gayane/BartLM/Bart/chemical/checkpoints/chemical/tokenizer/chem.model")
+                                #  data_name_or_path= "/home/gayane/BartLM/Bart/chemical/evaluation_data/sampl/processed",
+                                 sentencepiece_model="/home/gayane/BartLM/Bart/chemical/tokenizer/chem.model")
 bart.eval()
-bart.cuda()
+bart.cuda(device=1)
 
 
-# save the metrics for the run to a csv file
-# metrics_dataframe = run.history()
-# metrics_dataframe.to_csv("metrics.csv")
-
-
-input_dict = Dictionary.load(f"/home/gayane/BartLM/fairseq/processed/input0/dict.txt")
-# input_dict.add_symbol("<mask>")
-target_dict = Dictionary.load(f"/home/gayane/BartLM/fairseq/processed/label/dict.txt")
-
-
-# input_dict = Dictionary.load(f"/home/gayane/BartLM/Bart/chemical/checkpoints/chemical/evaluation_data/{dataset}/processed/input0/dict.txt")
-# input_dict.add_symbol("<mask>")
-# target_dict = Dictionary.load(f"/home/gayane/BartLM/Bart/chemical/checkpoints/chemical/evaluation_data/{dataset}/processed/label/dict.txt")
+input_dict = Dictionary.load(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}/processed/input0/dict.txt")
 
 smiles = list(load_indexed_dataset(
-    f"/home/gayane/BartLM/fairseq/processed/input0/test", input_dict))
-targets = list(load_indexed_dataset(
-    f"/home/gayane/BartLM/fairseq/processed/label/test", target_dict))
+    f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}/processed/input0/test", input_dict))
+test_label_path = f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}/processed/label/test"
+
+if task_type == 'classification':
+    target_dict = Dictionary.load(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{dataset}/processed/label/dict.txt")
+    targets = list(load_indexed_dataset(test_label_path, target_dict))
+elif task_type == 'regression':
+    with open(f'{test_label_path}.label') as f:
+        lines = f.readlines()
+        targets = [float(x.strip()) for x in lines]
 
 y_pred = []
 y = []
 for i, (smile, target) in tqdm(list(enumerate(zip(smiles, targets)))):
     smile = torch.cat((torch.cat((torch.tensor([0]), smile[:126])), torch.tensor([2])))
-    target = target[0].item()
-    y_pred.append(bart.predict('sentence_classification_head', smile)[0][1].exp().item())
-    y.append(target - 4)
+    output = bart.predict('sentence_classification_head', smile)
+    
+    if task_type =="classification":
+        target = target[0].item()
+        y_pred.append(output[0][1].exp().item())
+        y.append(target - 4)
+        
+    elif task_type == "regression":
+        y_pred.append(output[0][0].item())
+        y.append(target)
 
-print("ROC_AUC_SCORE: ", roc_auc_score(y, y_pred))
-print(classification_report(y, [int(x+0.5) for x in y_pred]))
-print(mean_squared_error())
+if task_type == 'classification':
+    print("ROC_AUC_SCORE: ", roc_auc_score(y, y_pred))
+    print(classification_report(y, [int(x+0.5) for x in y_pred]))
+else:
+    print(mean_squared_error([(ma -mi)*x + mi  for x in y] ,  [(ma -mi)*x +mi  for x in y_pred]))
+
 
--- scripts/process_moleculenet.py
+++ scripts/process_moleculenet.py
@@ -7,8 +7,8 @@ from ast import arg
 import numpy as np
 import deepchem as dc
 import pandas as pd
-import pandas as pd
 import argparse
+import json
 import os
 
 p = argparse.ArgumentParser(description=__doc__,
@@ -16,23 +16,28 @@ p = argparse.ArgumentParser(description=__doc__,
 
 p.add_argument("--input", help="input file", type=str)
 p.add_argument("--dataset-name", type=str, default="delaney")
-p.add_argument("--smile-index", type=int, default=-1)
-p.add_argument("--class-index", type=int)
-p.add_argument("--task-count", type=int, default=1)
-
+# p.add_argument("--smile-index", type=int, default=-1)
+# p.add_argument("--class-index",  type=int, default=-2)
+# p.add_argument("--task-count", type=int, default=1)
+# p.add_argument("")
 p.add_argument("--delimiter", type=str, default=",")
-p.add_argument("--task-type", type=str, default='classification')
+# p.add_argument("--task-type", type=str, default='classification')
 
-p.add_argument("--split-type", type=str, default='random')
-p.add_argument("--filter", type=bool, default=False)
+# p.add_argument("--split-type", type=str, default='random')
+# p.add_argument("--filter", type=bool, default=False)
 args = p.parse_args()
-
 
 np.random.seed(123)
 
-v = eval(f"dc.molnet.load_{args.dataset_name}")
-tasks, datasets, transformers = v(splitter=args.split_type,
-                                        featurizer = 'ECFP')
+with open('/home/gayane/BartLM/fairseq/scripts/datasets.json') as f:
+    datasets_json = json.load(f)
+
+dataset = datasets_json[args.dataset_name]
+
+v = eval(f"dc.molnet.load_{dataset['load_name']}")
+tasks, datasets, transformers = v(splitter=dataset['split_type'],
+                                            featurizer = 'ECFP')
+
 train_data, valid_data, test_data = datasets
 
 
@@ -40,10 +45,10 @@ train_df = train_data.to_dataframe()
 valid_df = valid_data.to_dataframe() 
 test_df = test_data.to_dataframe() 
 
-os.system(f'mkdir {args.input}/{args.dataset_name}-bin')
-os.system(f'mkdir {args.input}/{args.dataset_name}-bin/{args.dataset_name}')
+os.system(f'mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}')
+os.system(f'mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/{args.dataset_name}')
 
-#Remove some not usefull comlumns
+# Remove some not usefull comlumns
 remove_cols = [col for col in valid_df.columns if 'X' in col]
 valid_df.drop(remove_cols, axis='columns', inplace=True)
 train_df.drop(remove_cols, axis='columns', inplace=True)
@@ -53,90 +58,104 @@ valid_df.drop(remove_cols, axis='columns', inplace=True)
 train_df.drop(remove_cols, axis='columns', inplace=True)
 test_df.drop(remove_cols, axis='columns', inplace=True)
 
-test_df.to_csv(f"{args.input}/{args.dataset_name}-bin/{args.dataset_name}/test_{args.dataset_name}.csv")
+print(train_df.head())
+test_df.to_csv(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/{args.dataset_name}/test_{args.dataset_name}.csv")
 
-train_df.to_csv(f"{args.input}/{args.dataset_name}-bin/{args.dataset_name}/train_{args.dataset_name}.csv")
+train_df.to_csv(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/{args.dataset_name}/train_{args.dataset_name}.csv")
 
-valid_df.to_csv(f"{args.input}/{args.dataset_name}-bin/{args.dataset_name}/valid_{args.dataset_name}.csv")
+valid_df.to_csv(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/{args.dataset_name}/valid_{args.dataset_name}.csv")
+
+si = dataset['smiles_index']
+
+if dataset['filter']:
+    assert len(dataset['class_index']) == 1, "We do not want to filter multi-task datasets."
+    ci = dataset['class_index'][0]
+
+    nan_value = float("NaN")
+    train_df.replace("", nan_value, inplace=True)
+    train_df.dropna(subset=[train_df.columns[ci], train_df.columns[si]], inplace=True)
+
+    valid_df.replace("", nan_value, inplace=True)
+    valid_df.dropna(subset=[valid_df.columns[ci], valid_df.columns[si]], inplace=True)
+
+    test_df.replace("", nan_value, inplace=True)
+    test_df.dropna(subset=[test_df.columns[ci], test_df.columns[si]], inplace=True)
 
 
 loc = valid_df.columns.get_loc
 
-# print(train_df.size)
-# if args.filter:
-#     nan_value = float("NaN")
-#     train_df.replace("", nan_value, inplace=True)
-#     train_df.dropna(subset=[train_df.iloc('y'), train_df.columns[args.smile_index]], inplace=True)
-#     test_df.replace("", nan_value, inplace=True)
-#     test_df.dropna(subset=[test_df.iloc('y'), test_df.columns[args.smile_index]], inplace=True)
-#     valid_df.replace("", nan_value, inplace=True)
-#     valid_df.dropna(subset=[valid_df[.iloc('y')], valid_df.columns[args.smile_index]], inplace=True)
-# print(train_df.size)
 
+if dataset["type"] == "regression":
+    assert len(dataset['class_index']) == 1, "Regression tasks are always single-task."
+    ci = dataset['class_index'][0]
+    mi = dataset['minimum']
+    ma = dataset['maximum']
 
-
-if args.task_type == "regression":
-    mi = min(list(map(int, train_df.iloc[:, args.class_index].tolist())))
-    ma = max(list(map(int, train_df.iloc[:, args.class_index].tolist())))
-
-    for i in [list(map(int, test_df.iloc[:, args.class_index].tolist())), list(map(int, test_df.iloc[:, args.class_index].tolist()))]:
-        if min(i) < mi:
-            mi = min(i)
-        if max(i) > ma:
-            ma = max(i)
-    class_train = [(i - mi)/(ma - mi) for i in list(map(int, train_df.iloc[:, args.class_index].tolist()))] 
-    class_test = [(i - mi)/(ma - mi) for i in list(map(int, test_df.iloc[:, args.class_index].tolist()))]
-    class_val = [(i - mi)/(ma - mi) for i in list(map(int, valid_df.iloc[:, args.class_index].tolist()))]
-    smiles_train = list(map(str, train_df.iloc[:, args.smile_index].tolist()))
-    smiles_train = list(map(str, train_df.iloc[:, args.smile_index].tolist()))
-    smiles_test = list(map(str, test_df.iloc[:, args.smile_index].tolist()))
-    smiles_val = list(map(str, valid_df.iloc[:, args.smile_index].tolist()))
-    print(f"Scale {args.task_type} dataset [0,1] interval")
+    class_train = [(i - mi)/(ma - mi) for i in list(train_df.iloc[:, ci].values.tolist())] 
+    class_test = [(i - mi)/(ma - mi) for i in list(test_df.iloc[:, ci].values.tolist())]
+    class_val = [(i - mi)/(ma - mi) for i in list(valid_df.iloc[:, ci].values.tolist())]
+    
+    print(f"Scale {args.dataset_name} (type={dataset['type']}) dataset [0,1] interval")
     print(f"Regression task target minimum value: {mi} and max value: {ma} ")
 
-if args.task_type == "classification":
-    if args.task_count >1:
-        class_train = list(train_df.iloc[:, loc('y1'):loc('y' + str(args.task_count))+1].values.tolist())
-        class_test = list(test_df.iloc[:, loc('y1'):loc('y' + str(args.task_count))+1].values.tolist())
-        class_val = list(valid_df.iloc[:, loc('y1'):loc('y' + str(args.task_count))+1].values.tolist())
+if dataset["type"] == "classification":
+    if len(dataset["class_index"]) >1:
+        class_dict = {}
+        for cii in range(1, len(dataset["class_index"])):
+            class_dict["class_train" +str(cii)] =list(map(int, train_df.iloc[:, dataset["class_index"][cii]].tolist()))
+            class_dict["class_val" +str(cii)] =list(map(int, valid_df.iloc[:, dataset["class_index"][cii]].tolist()))
+            class_dict["class_test" +str(cii)] =list(map(int, test_df.iloc[:, dataset["class_index"][cii]].tolist()))
+
     else:
-        class_train = list(train_df.iloc[:, loc('y')].values.tolist())
-        class_test = list(test_df.iloc[:, loc('y')].values.tolist())
-        class_val = list(valid_df.iloc[:, loc('y')].values.tolist())
+        ci = dataset['class_index'][0]
+        class_train = list(map(int, train_df.iloc[:, ci].tolist()))
+        class_val = list(map(int, valid_df.iloc[:, ci].tolist()))
+        class_test = list(map(int, test_df.iloc[:, ci].tolist()))
 
-    smiles_val = list(map(str, valid_df.iloc[:, args.smile_index].tolist()))
-    smiles_test = list(map(str, test_df.iloc[:, args.smile_index].tolist()))
-    smiles_train = list(map(str, train_df.iloc[:, args.smile_index].tolist()))
+smiles_train = list(map(str, train_df.iloc[:, si].tolist()))
+smiles_val = list(map(str, valid_df.iloc[:, si].tolist()))
+smiles_test = list(map(str, test_df.iloc[:, si].tolist()))
 
-
-if args.task_type =='regression':
-    os.system(f'mkdir {args.input}/{args.dataset_name}-bin/label')
-    with open(f"{args.input}/{args.dataset_name}-bin/label/train.label", "w") as f:
+if dataset["type"] =='regression':
+    os.system(f'mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/label')
+    with open(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/label/train.label", "w") as f:
         for item in class_train:
             f.write("%s\n" % item)
-    with open(f"{args.input}/{args.dataset_name}-bin/label/valid.label", "w") as f:
+    with open(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/label/valid.label", "w") as f:
         for item in class_val:
             f.write("%s\n" % item)
-    with open(f"{args.input}/{args.dataset_name}-bin/label/test.label", "w") as f:
-        for item in class_val:
+    with open(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/label/test.label", "w") as f:
+        for item in class_test:
             f.write("%s\n" % item)
 
-os.system(f"mkdir {args.input}/{args.dataset_name}-bin/raw")
-os.system(f"mkdir {args.input}/{args.dataset_name}-bin/tokenized")
-os.system(f"mkdir {args.input}/{args.dataset_name}-bin/processed")
-
-os.system(f'mkdir {args.input}/{args.dataset_name}-bin/processed/label')
-with open(f"{args.input}/{args.dataset_name}-bin/processed/label/train.label", "w") as f:
-    for item in class_train:
-        f.write("%s\n" % item)
-with open(f"{args.input}/{args.dataset_name}-bin/processed/label/valid.label", "w") as f:
-    for item in class_val:
-        f.write("%s\n" % item)
-with open(f"{args.input}/{args.dataset_name}-bin/processed/label/test.label", "w") as f:
-    for item in class_val:
-        f.write("%s\n" % item)
-
-
+os.system(f"mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/raw")
+os.system(f"mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/tokenized")
+os.system(f"mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed")
+if len(dataset['class_index'])>1:
+    for i in range(1, len(dataset['class_index'])):
+        
+        os.system(f'mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/label{i}')
+        with open(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/label{i}/train.label", "w") as f:
+            for item in  class_dict["class_train" +str(i)]:
+                f.write("%s\n" % item)
+        with open(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/label{i}/valid.label", "w") as f:
+            for item in class_dict["class_val" +str(i)]:
+                f.write("%s\n" % item)
+        with open(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/label{i}/test.label", "w") as f:
+            for item in class_dict["class_test" +str(i)]:
+                f.write("%s\n" % item)
+    
+else:
+    os.system(f'mkdir /home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/label')
+    with open(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/label/train.label", "w") as f:
+        for item in class_train:
+            f.write("%s\n" % item)
+    with open(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/label/valid.label", "w") as f:
+        for item in class_val:
+            f.write("%s\n" % item)
+    with open(f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/label/test.label", "w") as f:
+        for item in class_test:
+            f.write("%s\n" % item)
 
 print(f"{args.dataset_name} Train Length: {len(smiles_train)}")
 print(f"{args.dataset_name} Valid Length: {len(smiles_val)}")
@@ -147,13 +166,12 @@ X_splits = []
 y_splits = []
 
 
-
 # Write Raw Splits
 print("Writing Input Splits")
 for name, smiles in zip(names, (smiles_train, smiles_val, smiles_test)):
     print(name + ".target")
     print(args.dataset_name )
-    new_path = f"{args.input}/{args.dataset_name}-bin/raw/" + name + ".input" 
+    new_path = f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/raw/" + name + ".input" 
 
     X_splits.append(new_path)
     with open(new_path, "w+") as f:
@@ -161,14 +179,26 @@ for name, smiles in zip(names, (smiles_train, smiles_val, smiles_test)):
             f.write(f"{smile}\n")
 
 print("Writing Output Splits")
-for name, targets in zip(names, (class_train, class_val, class_test)):
-    print( f"{args.input}/{args.dataset_name}-bin/raw/" + name + ".target")
-    print(args.dataset_name )
-    new_path = f"{args.input}/{args.dataset_name}-bin/raw/" + name + ".target"
-    y_splits.append(new_path)
-    with open(new_path, "w+") as f:
-        for target in targets:
-            f.write(f"{str(target)}\n")
+if len(dataset['class_index'])>1:
+    pass
+    # for i in range(1, len(dataset['class_index'])):
+    #     for name, targets in zip(names, (class_dict["class_train" +str(i)], class_dict["class_val" +str(i)], class_dict["class_test" +str(i)])):
+    #         print( f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/raw/{i}" + name + ".target")
+    #         print(args.dataset_name )
+    #         new_path = f"/home/gayane/BartLM/Bart/chemyical/evaluation_data/{args.dataset_name}/raw/{i}" + name + ".target"
+    #         y_splits.append(new_path)
+    #         with open(new_path, "w+") as f:
+    #             for target in targets:
+    #                 f.write(f"{str(target)}\n")
+else:
+    for name, targets in zip(names, (class_train, class_val, class_test)):
+        print( f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/raw/" + name + ".target")
+        print(args.dataset_name )
+        new_path = f"/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/raw/" + name + ".target"
+        y_splits.append(new_path)
+        with open(new_path, "w+") as f:
+            for target in targets:
+                f.write(f"{str(target)}\n")
 
 # Tokenize Texts
 print("Tokenizing")
@@ -179,7 +209,7 @@ for path in X_splits:
     print(cur_path)
     splits.append(cur_path)
     os.system(
-        f"python {args.input}/fairseq/scripts/spm_parallel.py --input {path} --outputs {cur_path} --model /home/gayane/BartLM/Bart/chemical/checkpoints/chemical/tokenizer/chem.model")
+        f"python {args.input}/fairseq/scripts/spm_parallel.py --input {path} --outputs {cur_path} --model /home/gayane/BartLM/Bart/chemical/tokenizer/chem.model")
 
 X_splits = splits
 
@@ -187,14 +217,25 @@ os.system(('fairseq-preprocess --only-source '
            f'--trainpref "{X_splits[0]}" '
            f'--validpref "{X_splits[1]}" '
            f'--testpref "{X_splits[2]}" '
-           f'--destdir "{args.input}/{args.dataset_name}-bin/processed/input0" --workers 60 '
-           '--srcdict /home/gayane/BartLM/Bart/chemical/checkpoints/chemical/tokenizer/chem.vocab.fs'))
-if args.task_type == "classification":
-    os.system(('fairseq-preprocess '
-           '--only-source '
-           f'--trainpref "{y_splits[0]}" '
-           f'--validpref "{y_splits[1]}" '
-           f'--testpref "{y_splits[2]}" '
-           f'--destdir "{args.input}/{args.dataset_name}-bin/processed/label" --workers 60 '))
+           f'--destdir "/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/input0" --workers 60 '
+           '--srcdict /home/gayane/BartLM/Bart/chemical/tokenizer/chem.vocab.fs'))
+if dataset["type"] == "classification":
+    if len(dataset["class_index"]) >1:
+        for i in range(1, len(dataset['class_index'])):
+            os.system(('fairseq-preprocess '
+            '--only-source '
+            f'--trainpref "{class_dict["class_train" +str(i)][0]}" '
+            f'--validpref "{class_dict["class_val" +str(i)][1]}" '
+            f'--testpref "{class_dict["class_test" +str(i)][2]}" '
+            f'--destdir "/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/label{i}" --workers 60 '))
+
+    
+    else:
+        os.system(('fairseq-preprocess '
+            '--only-source '
+            f'--trainpref "{y_splits[0]}" '
+            f'--validpref "{y_splits[1]}" '
+            f'--testpref "{y_splits[2]}" '
+            f'--destdir "/home/gayane/BartLM/Bart/chemical/evaluation_data/{args.dataset_name}/processed/label" --workers 60 '))
 
     
