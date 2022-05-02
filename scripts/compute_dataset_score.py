b3e376b4f1ab900e97a4955e32ca1186b39d23c0
gayanechilingar <gayane.chilingar@gmail.com>
2022-05-02 20:24:40 +0400

    add learning rate in command

--- scripts/compute_dataset_score.py
+++ scripts/compute_dataset_score.py
@@ -5,20 +5,21 @@ import os
 
 
 
-with open('/home/hrant/YerevaNN Chemical Results - Apr25.csv') as f:
+with open('/home/gayane/BartLM/fairseq/scripts/YerevaNN Chemical Results - Apr25.csv') as f:
     r = csv.DictReader(f)
     lines = [row for row in r]
-
+    print(f"------------> {lines}")
 
 for task in tqdm(lines):
     task_name = task['']
     subtask_count = int(task['# of subtasks'])
+    lr = float(task['lr'])
     TOTAL_NUM_UPDATES = int(float(task['# of steps']))
     WARMUP_UPDATES = int(0.16 * TOTAL_NUM_UPDATES)
     for subtask in range(subtask_count):
 
         print(f"\nStarting subtask {int(subtask)}")
 
-        cmd = f"python /home/gayane/BartLM/fairseq/scripts/compute_auc.py --dataset-name {task_name} --subtask {str(int(subtask))} --warmup-update  {WARMUP_UPDATES} --total-number-update {TOTAL_NUM_UPDATES} " 
+        cmd = f"python /home/gayane/BartLM/fairseq/scripts/compute_auc.py --lr {lr} --dataset-name {task_name} --subtask {str(int(subtask))} --warmup-update  {WARMUP_UPDATES} --total-number-update {TOTAL_NUM_UPDATES} " 
         print(cmd)
         os.system(cmd)
